49ecd2d8e3f2510cf59266f8d6fcf94dbc06207e
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-06-05 21:31:33 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+####################################
+#        RULLES OF PRODUCTION      #
+#  1. MODULARITY!!!                #
+#  2. ABC : Always Be Committing   #
+#  3. 1 feature = 1 branch         #
+#  4. use type annotation          #
+#  5. plan before coding    	   #
+#  6. unit tests for all functions #
+####################################
+
+if __name__ == "__main__":
+    print("Hello, World!")
