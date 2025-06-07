e7178ef68cd32b34f4c28d7c08247eec842c62c3
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-06-07 15:54:43 +0200

    local maximum function

--- utils/Utils.py
+++ utils/Utils.py
@@ -3,3 +3,7 @@ from include import TOLERANCE
 
 def isSameEvent(x1 :int,x2 :int):
     return (x1 - TOLERANCE) <= x2 <= (x1 + TOLERANCE)
+
+
+def findLocalMaximum(buffer :list[int]):
+    return max(buffer)
