b186b69e9a6cb4512123bef6e7e3bf158d093b4c
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-06-09 10:00:26 +0200

    improved the layout of the print channels funciton

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ from detection.Detection import detectChannelsEOGEvents
 from evaluation.Evaluation import batchTest
 
 if __name__ == "__main__":
-    # filepath :str = r'C:/Users/gorga/CodeProjects/Arduino/Blink/projet_blink/data/saccade_right_nathan.xdf'
-    # selected_channels: list[int] = [1,3]
+    filepath :str = r'C:/Users/gorga/CodeProjects/Arduino/Blink/projet_blink/data/saccade_right_nathan.xdf'
+    selected_channels: list[int] = [1,3]
 
-    # [ channels, sfreq ] = extractChannelsFromXdf(filepath,selected_channels, extract_sfreq=True)
+    [ channels, sfreq ] = extractChannelsFromXdf(filepath,selected_channels, extract_sfreq=True)
 
-    # chosen_channels = arange(0,len(selected_channels))
+    chosen_channels = arange(0,len(selected_channels))
         
-    # eog_events = detectChannelsEOGEvents(channels,sfreq)
+    eog_events = detectChannelsEOGEvents(channels,sfreq)
 
-    # printData(channels, chosen_channels, viewType="detected_eogs",eog_events=eog_events)
+    printData(channels, chosen_channels, viewType="detected_eogs",eog_events=eog_events)
     
-    folder_path = r'evaluation\json_folder'
-    batchTest(folder_path)
+    # folder_path = r'evaluation\json_folder'
+    # successes, failures = batchTest(folder_path)
--- visualize/Visualize.py
+++ visualize/Visualize.py
@@ -6,36 +6,54 @@ color = ["blue", "green", "red", "purple", "orange", "brown", "pink", "gray"]
 def printChannels(channels :list[list[float]]):
     num_channels = len(channels)
 
-    _, axs = plt.subplots(num_channels, 1, figsize=(10, 3 * num_channels), sharex=True)
+    col = (2 // num_channels)
+
+    _, axs = plt.subplots(num_channels, col, figsize=(10, 3 * num_channels), sharex=True)
 
     # If only 1 channel, axs is not a list
     if num_channels == 1:
         axs = [axs]
 
-    for i, data in enumerate(channels):
-        axs[i].plot(range(len(data)), data, color=color[i % len(color)])
-        axs[i].scatter(range(len(data)), data, color=color[i % len(color)],s=10)
-        axs[i].set_ylabel(f"Channel {i+1}")
-        axs[i].grid(True)
 
-    axs[-1].set_xlabel("Sample Index")
-    return axs
+    if col == 1:
+        for i, data in enumerate(channels):
+            axs[i].plot(range(len(data)), data, color=color[i % len(color)])
+            axs[i].scatter(range(len(data)), data, color=color[i % len(color)],s=10)
+            axs[i].set_ylabel(f"Channel {i+1}")
+            axs[i].grid(True)
+    else:
+        for c in range(col):
+            for i, data in enumerate(channels):
+                axs[i][c].plot(range(len(data)), data, color=color[i % len(color)])
+                axs[i][c].scatter(range(len(data)), data, color=color[i % len(color)],s=10)
+                axs[i][c].set_ylabel(f"Channel {i+1}")
+                axs[i][c].grid(True)
+
+    return axs, num_channels, col
 
 
 def printDetectedEOGs(channels : list[list[float]], eog_events):
     
     assert len(channels) == len(eog_events)
     
-    axs = printChannels(channels)
+    axs, row , col = printChannels(channels)
 
-    sizeChannels = len(channels)
+    sizeChannels = row
     sizeColor = len(color)
-    for i in range(sizeChannels):
-        sizeEvents = len(eog_events[i])
-        for j in range(sizeEvents):
-            axs[i].axvline(x=eog_events[i][j], color=color[((i+1)%sizeColor)] , linestyle='--', linewidth=2)
-    return axs
+    for c in range(col):
+        for i in range(sizeChannels):
+            sizeEvents = len(eog_events[i])
+            for j in range(sizeEvents):
+                if col == 1:
+                    axs[i].axvline(x=eog_events[i][j], color=color[((i+1)%sizeColor)] , linestyle='--', linewidth=2)
+                
+                else:
+                    axs[i][c].axvline(x=eog_events[i][j], color=color[((i+1)%sizeColor)] , linestyle='--', linewidth=2)
+    return axs, row, col
         
+
+def printTest(test_data : list[list[float]], test_succeed=False):
+    print("TODO")
 
 
 def printData(
@@ -58,9 +76,13 @@ def printData(
         
     match viewType:
         case "channel_only":
-            _ = printChannels(send_channels)
+            _,_,_ = printChannels(send_channels)
         case "detected_eogs":
-            _ = printDetectedEOGs(send_channels,eog_events)
+            _,_,_ = printDetectedEOGs(send_channels,eog_events)
+        # case "test_success":
+        #     # _ = printTest()
+        # case "test_fail":
+        #     # _ = printTest()
     
     plt.tight_layout()
     plt.show()
