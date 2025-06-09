05582cbddd4ec5311dd0890fb96a1e0fd7b2e5f0
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-06-09 11:44:16 +0200

    this is the not useful

--- main.py
+++ main.py
@@ -16,38 +16,18 @@ from detection.Detection import detectChannelsEOGEvents
 from evaluation.Evaluation import batchTest, getTestData
 
 if __name__ == "__main__":
-    # filepath :str = r'C:/Users/gorga/CodeProjects/Arduino/Blink/projet_blink/data/saccade_right_nathan.xdf'
-    # selected_channels: list[int] = [1,3]
+    filepath :str = r'C:/Users/gorga/CodeProjects/Arduino/Blink/projet_blink/data/saccade_right_nathan.xdf'
+    selected_channels: list[int] = [1,3]
 
-    # [ channels, sfreq ] = extractChannelsFromXdf(filepath,selected_channels, extract_sfreq=True)
+    [ channels, sfreq ] = extractChannelsFromXdf(filepath,selected_channels, extract_sfreq=True)
 
-    # chosen_channels = arange(0,len(selected_channels))
+    chosen_channels = arange(0,len(selected_channels))
         
-    # eog_events = detectChannelsEOGEvents(channels,sfreq)
+    eog_events = detectChannelsEOGEvents(channels,sfreq)
 
-    # printData(channels, chosen_channels, viewType="detected_eogs",eog_events=eog_events)
+    printData(channels, chosen_channels, viewType="detected_eogs",eog_events=eog_events)
     
     
-    
-    
-    folder_path = r'evaluation\json_folder'
-
-    batchTest(folder_path)
-
-
-    tests = getTestData(folder_path)
-    
-    
-    print(tests)
-    # successes = []
-    # failures = []
-    
-    # for test in tests: 
-    #     if test["outcome"]["result"] == "PASS":
-    #         successes.append(test)
-    #     else:
-    #         failures.append(test)
-
     
     
     
